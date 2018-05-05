4395c51317f439cd89948af2e5c168d2c2e861f8
Shintaro Ishida <sh1sh1dax@gmail.com>
2018-05-05 12:51:14 +0900

    オセロとしてとりあえず動くようにした

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ class Game extends React.Component {
     }
 
     const putStone = (this.state.blackIsNext ? BLACK_STONE : WHITE_STONE);
-    const reversed = reverseStones(x, y, putStone, squares);
+    const reversed = reverseStones(x, y, putStone, squares, false);
     if (reversed == 0) {
       return;
     }
@@ -107,7 +107,18 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+
+    const currentStone = this.state.blackIsNext ? BLACK_STONE : WHITE_STONE;
+    const nextStone = this.state.blackIsNext ? WHITE_STONE : BLACK_STONE;
+    let isGameOver = false;
+    if (!isPuttable(current.squares, currentStone)) {
+      if (!isPuttable(current.squares, nextStone)) {
+        isGameOver = true;
+      } else {
+        // pass
+        this.state.blackIsNext = !this.state.blackIsNext;
+      }
+    }
 
     const moves = history.map((step, move) => {
       const desc = move ?
@@ -121,8 +132,30 @@ class Game extends React.Component {
     });
 
     let status;
-    if (winner) {
-      status = "Winner: " + winner;
+    if (isGameOver) {
+      let nBlacks = 0;
+      let nWhites = 0;
+
+      for (let y = 0; y < current.squares.length; y++) {
+        for (let x = 0; x < current.squares[y].length; x++) {
+          if (current.squares[y][x] === BLACK_STONE) {
+            nBlacks++;
+          } else if (current.squares[y][x] === WHITE_STONE) {
+            nWhites++;
+          }
+        }
+      }
+
+      let winner;
+      if (nBlacks === nWhites) {
+        winner = "Draw!";
+      } else if (nBlacks > nWhites) {
+        winner = BLACK_STONE;
+      } else if (nBlacks < nWhites) {
+        winner = WHITE_STONE;
+      }
+
+      status = 'Winner: ' + winner + ' （ ' + BLACK_STONE + ' ' + nBlacks + ' ' + WHITE_STONE + ' ' + nWhites + ' ） ';
     } else {
       status = 'Next player: ' + (this.state.blackIsNext ? BLACK_STONE : WHITE_STONE);
     }
@@ -151,179 +184,239 @@ ReactDOM.render(
 );
 
 function calculateWinner(squares) {
-  return;
+  let nBlacks = 0;
+  let nWhites = 0;
+
+  for (let y = 0; y < squares.length; y++) {
+    for (let x = 0; x < squares[y].length; x++) {
+      if (squares[y][x] === BLACK_STONE) {
+        nBlacks++;
+      } else if (squares[y][x] === WHITE_STONE) {
+        nWhites++;
+      }
+    }
+  }
+
+  return BLACK_STONE;
 }
 
-function reverseStones(x, y, putStone, squares) {
+function isPuttable(squares, putStone) {
+  for (let y = 0; y < squares.length; y++) {
+    for (let x = 0; x < squares[y].length; x++) {
+      if (squares[y][x] !== null) {
+        continue;
+      }
+      const reversibleStones = reverseStones(x, y, putStone, squares, true);
+      if (reversibleStones > 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function reverseStones(x, y, putStone, squares, isCheckOnly) {
   let reversed = 0;
 
   // left
   if (x !== 0 && squares[y][x - 1] && squares[y][x - 1] !== putStone) {
-    reversed += reverseLeft(x - 1, y, putStone, squares);
+    reversed += reverseLeft(x - 1, y, putStone, squares, isCheckOnly);
   }
   // right
   if (x !== BOARD_WIDTH - 1 && squares[y][x + 1] && squares[y][x + 1] !== putStone) {
-    reversed += reverseRight(x + 1, y, putStone, squares);
+    reversed += reverseRight(x + 1, y, putStone, squares, isCheckOnly);
   }
   // up
   if (y !== 0 && squares[y - 1][x] && squares[y - 1][x] !== putStone) {
-    reversed += reverseUp(x, y - 1, putStone, squares);
+    reversed += reverseUp(x, y - 1, putStone, squares, isCheckOnly);
   }
   // down
   if (y !== BOARD_HEIGHT - 1 && squares[y + 1][x] && squares[y + 1][x] !== putStone) {
-    reversed += reverseDown(x, y + 1, putStone, squares);
+    reversed += reverseDown(x, y + 1, putStone, squares, isCheckOnly);
   }
   // left up
   if (x !== 0 && y !== 0 && squares[y - 1][x - 1] && squares[y - 1][x - 1] !== putStone) {
-    reversed += reverseLeftUp(x - 1, y - 1, putStone, squares);
+    reversed += reverseLeftUp(x - 1, y - 1, putStone, squares, isCheckOnly);
   }
   // right up
   if (x !== BOARD_WIDTH - 1 && y !== 0 && squares[y - 1][x + 1] && squares[y - 1][x + 1] !== putStone) {
-    reversed += reverseRightUp(x + 1, y - 1, putStone, squares);
+    reversed += reverseRightUp(x + 1, y - 1, putStone, squares, isCheckOnly);
   }
   // right down
   if (x !== BOARD_WIDTH - 1 && y !== BOARD_HEIGHT - 1 && squares[y + 1][x + 1] && squares[y + 1][x + 1] !== putStone) {
-    reversed += reverseRightDown(x + 1, y + 1, putStone, squares);
+    reversed += reverseRightDown(x + 1, y + 1, putStone, squares, isCheckOnly);
   }
   // left down
   if (x !== 0 && y !== BOARD_HEIGHT - 1 && squares[y + 1][x - 1] && squares[y + 1][x - 1] !== putStone) {
-    reversed += reverseLeftDown(x - 1, y + 1, putStone, squares);
+    reversed += reverseLeftDown(x - 1, y + 1, putStone, squares, isCheckOnly);
   }
 
   return reversed;
 }
 
-function reverseLeft(x, y, putStone, squares) {
-  if (x <= 0 || squares[y][x] === putStone) {
+function reverseLeft(x, y, putStone, squares, isCheckOnly) {
+  if (x <= 0 || squares[y][x] === putStone || squares[y][x] === null) {
     return 0;
   }
   if (squares[y][x - 1] && squares[y][x - 1] === putStone) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     return 1;
   }
 
-  let reversed = reverseLeft(x - 1, y, putStone, squares);
+  let reversed = reverseLeft(x - 1, y, putStone, squares, isCheckOnly);
   if (reversed > 0) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     reversed++;
   }
   return reversed;
 }
 
-function reverseRight(x, y, putStone, squares) {
-  if (x >= BOARD_WIDTH - 1 || squares[y][x] === putStone) {
+function reverseRight(x, y, putStone, squares, isCheckOnly) {
+  if (x >= BOARD_WIDTH - 1 || squares[y][x] === putStone || squares[y][x] === null) {
     return 0;
   }
   if (squares[y][x + 1] && squares[y][x + 1] === putStone) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     return 1;
   }
 
-  let reversed = reverseRight(x + 1, y, putStone, squares);
+  let reversed = reverseRight(x + 1, y, putStone, squares, isCheckOnly);
   if (reversed > 0) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     reversed++;
   }
   return reversed;
 }
 
-function reverseUp(x, y, putStone, squares) {
-  if (y <= 0 || squares[y][x] === putStone) {
+function reverseUp(x, y, putStone, squares, isCheckOnly) {
+  if (y <= 0 || squares[y][x] === putStone || squares[y][x] === null) {
     return 0;
   }
   if (squares[y - 1][x] && squares[y - 1][x] === putStone) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     return 1;
   }
 
-  let reversed = reverseUp(x, y - 1, putStone, squares);
+  let reversed = reverseUp(x, y - 1, putStone, squares, isCheckOnly);
   if (reversed > 0) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     reversed++;
   }
   return reversed;
 }
 
-function reverseDown(x, y, putStone, squares) {
-  if (y >= BOARD_HEIGHT - 1 || squares[y][x] === putStone) {
+function reverseDown(x, y, putStone, squares, isCheckOnly) {
+  if (y >= BOARD_HEIGHT - 1 || squares[y][x] === putStone || squares[y][x] === null) {
     return 0;
   }
   if (squares[y + 1][x] && squares[y + 1][x] === putStone) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     return 1;
   }
 
-  let reversed = reverseDown(x, y + 1, putStone, squares);
+  let reversed = reverseDown(x, y + 1, putStone, squares, isCheckOnly);
   if (reversed > 0) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     reversed++;
   }
   return reversed;
 }
 
-function reverseLeftUp(x, y, putStone, squares) {
-  if (x <= 0 || y <= 0 || squares[y][x] === putStone) {
+function reverseLeftUp(x, y, putStone, squares, isCheckOnly) {
+  if (x <= 0 || y <= 0 || squares[y][x] === putStone || squares[y][x] === null) {
     return 0;
   }
   if (squares[y - 1][x - 1] && squares[y - 1][x - 1] === putStone) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     return 1;
   }
 
-  let reversed = reverseLeftUp(x - 1, y - 1, putStone, squares);
+  let reversed = reverseLeftUp(x - 1, y - 1, putStone, squares, isCheckOnly);
   if (reversed > 0) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     reversed++;
   }
   return reversed;
 }
 
-function reverseRightUp(x, y, putStone, squares) {
-  if (x >= BOARD_WIDTH - 1 || y <= 0 || squares[y][x] === putStone) {
+function reverseRightUp(x, y, putStone, squares, isCheckOnly) {
+  if (x >= BOARD_WIDTH - 1 || y <= 0 || squares[y][x] === putStone || squares[y][x] === null) {
     return 0;
   }
   if (squares[y - 1][x + 1] && squares[y - 1][x + 1] === putStone) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     return 1;
   }
 
-  let reversed = reverseRightUp(x + 1, y - 1, putStone, squares);
+  let reversed = reverseRightUp(x + 1, y - 1, putStone, squares, isCheckOnly);
   if (reversed > 0) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     reversed++;
   }
   return reversed;
 }
 
-function reverseRightDown(x, y, putStone, squares) {
-  if (x >= BOARD_WIDTH - 1 || y >= BOARD_HEIGHT - 1 || squares[y][x] === putStone) {
+function reverseRightDown(x, y, putStone, squares, isCheckOnly) {
+  if (x >= BOARD_WIDTH - 1 || y >= BOARD_HEIGHT - 1 || squares[y][x] === putStone || squares[y][x] === null) {
     return 0;
   }
   if (squares[y + 1][x + 1] && squares[y + 1][x + 1] === putStone) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     return 1;
   }
 
-  let reversed = reverseRightDown(x + 1, y + 1, putStone, squares);
+  let reversed = reverseRightDown(x + 1, y + 1, putStone, squares, isCheckOnly);
   if (reversed > 0) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     reversed++;
   }
   return reversed;
 }
 
-function reverseLeftDown(x, y, putStone, squares) {
-  if (x <= 0 || y >= BOARD_HEIGHT - 1 || squares[y][x] === putStone) {
+function reverseLeftDown(x, y, putStone, squares, isCheckOnly) {
+  if (x <= 0 || y >= BOARD_HEIGHT - 1 || squares[y][x] === putStone || squares[y][x] === null) {
     return 0;
   }
   if (squares[y + 1][x - 1] && squares[y + 1][x - 1] === putStone) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     return 1;
   }
 
-  let reversed = reverseLeftDown(x - 1, y + 1, putStone, squares);
+  let reversed = reverseLeftDown(x - 1, y + 1, putStone, squares, isCheckOnly);
   if (reversed > 0) {
-    squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    if (!isCheckOnly) {
+      squares[y][x] = (putStone === BLACK_STONE ? BLACK_STONE : WHITE_STONE);
+    }
     reversed++;
   }
   return reversed;
