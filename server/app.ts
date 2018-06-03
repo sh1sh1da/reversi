92830e52bdf9f1ceeec1b46a2046dcee59660362
Shintaro Ishida <sh1sh1dax@gmail.com>
2018-06-03 21:55:42 +0900

    サーバサイドでTypeScript使うようにした

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,13 @@
-var express = require('express');
-var app = express();
+import { Server } from 'net';
 
-server = app.listen(8080, function () {
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const server: Server = app.listen(8080, function () {
   console.log('server is running on port 8080')
 });
-
-var socket = require('socket.io');
-io = socket(server);
-
+const io = socket(server);
 let gameState = null;
 
 io.on('connection', (socket) => {
